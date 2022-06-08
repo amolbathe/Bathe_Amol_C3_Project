a613e00649683731e3ded987a7e085c66c539f81
abathe90 <amolbathe@outlook.com>
2022-06-08 21:34:37 +0530

    Part 3: Failing test case

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -1,67 +1,98 @@
-import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.time.*;
 
-public class Restaurant {
-    private String name;
-    private String location;
-    public LocalTime openingTime;
-    public LocalTime closingTime;
-    private List<Item> menu = new ArrayList<Item>();
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
-    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
-        this.name = name;
-        this.location = location;
-        this.openingTime = openingTime;
-        this.closingTime = closingTime;
+class RestaurantTest {
+    Restaurant restaurant;
+    //REFACTOR ALL THE REPEATED LINES OF CODE COMPLETED
+    @BeforeEach
+    public void setup(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
     }
-    //Task 1.2 Completed
-    public boolean isRestaurantOpen() {
-        return getCurrentTime().isAfter(openingTime) && getCurrentTime().isBefore(closingTime);
-    }
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
-
-    // Task 1.1 Completed
-    public List<Item> getMenu() {
-        return this.menu;
-
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        Restaurant spiedRestaurant = Mockito.spy(restaurant);
+        Mockito.when(spiedRestaurant.getCurrentTime()).thenReturn(LocalTime.of( 13 , 20 ));
+        assertTrue(spiedRestaurant.isRestaurantOpen(), "Restaurant Open Test Failed");
     }
 
-    private Item findItemByName(String itemName){
-        for(Item item: menu) {
-            if(item.getName().equals(itemName))
-                return item;
-        }
-        return null;
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+        Restaurant spiedRestaurant = Mockito.spy(restaurant);
+        Mockito.when(spiedRestaurant.getCurrentTime()).thenReturn(LocalTime.of( 23 , 55 ));
+        assertFalse(spiedRestaurant.isRestaurantOpen(), "Restaurant Closed Test Failed");
     }
 
-    public void addToMenu(String name, int price) {
-        Item newItem = new Item(name,price);
-        menu.add(newItem);
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
     }
-
-    public void removeFromMenu(String itemName) throws itemNotFoundException {
-
-        Item itemToBeRemoved = findItemByName(itemName);
-        if (itemToBeRemoved == null)
-            throw new itemNotFoundException(itemName);
-
-        menu.remove(itemToBeRemoved);
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
     }
-    public void displayDetails(){
-        System.out.println("Restaurant:"+ name + "\n"
-                +"Location:"+ location + "\n"
-                +"Opening time:"+ openingTime +"\n"
-                +"Closing time:"+ closingTime +"\n"
-                +"Menu:"+"\n"+getMenu());
-
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
     }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    //Test for Part 3 /TDD Part
+    @Test
+    public void totalPrice_should_be_equal_to_total_price_of_added_items() {
 
-    public String getName() {
-        return name;
+        List<String> selectedItems = new ArrayList<String>();
+        selectedItems.add("Sweet corn soup");
+        selectedItems.add("Vegetable lasagne");
+
+        int actualAmount = restaurant.calculateAmount(selectedItems);
+        assertEquals(388, actualAmount);
     }
+    @Test
+    public void totalPrice_should_not_be_random_or_fixed_value() {
 
+        List<String> selectedItems = new ArrayList<String>();
+        selectedItems.add("Sweet corn soup");
+        int actualAmount = restaurant.calculateAmount(selectedItems);
+        assertNotEquals(388, actualAmount);
+    }
+    @Test
+    public void totalPrice_should_not_be_less_than_total_price_of_items() {
+
+        List<String> selectedItems = new ArrayList<String>();
+        selectedItems.add("Sweet corn soup");
+        selectedItems.add("Vegetable lasagne");
+
+        int actualAmount = restaurant.calculateAmount(selectedItems);
+        assertFalse(actualAmount<388,"Actual total price not less than total item's price test failed");
+    }
+    //Test for Part 3 /TDD Part
 }
